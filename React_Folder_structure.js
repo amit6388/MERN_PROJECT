4befa90b3fa75f903a95ac18a19afbc36d464851
amit6388 <amitpoly2020@gmail.com>
2024-09-12 23:29:36 +0530

    new changes

---
+++ React_Folder_structure.js
@@ -0,0 +1,51 @@
+
+/*==================================
+
+
+Please  suggest a project name for the project we are going to create.
+
+The names are below also you can suggest your own project name:-
+
+1.RecruitX
+2.WorkPlaceX 
+3.JobNavigator
+4.CareerSphere
+5.CareerSuite
+6.CareerGate
+7.CareerPath
+8.TalentHunt
+
+
+
+========================================================================= 
+
+
+
+PLEASE HAVE A LOOK FOLDER STRUCTURE OF OUR React PROJECT 
+                            ===>
+
+RecruitX    #PROJECT NAME
+├── node_modules/
+├── public/    # yaha static file aur photos rhega jo project me use krenge hm log.
+├── src/
+│   ├── assets/          # Optional: where you can store images, fonts, etc.
+│   ├── components/
+│   │   ├── landingpage/
+│   │   │   └── [LandingPage related Components] #LandingPage ka code yaha rhega
+│   │   ├── admin/
+│   │   │   └── [Admin related Components]  #Admin ka code yaha rhega
+│   │   ├── jobseeker/
+│   │   │   └── [JobSeeker related Components] #jobseeker ka code yaha rhega
+│   │   ├── recruiter/
+│   │   │   └── [Recruiter related Components ] #recruiter ka code yaha rhega
+│   ├── App.jsx          # Main application file
+|   ├── App.css          # Main css  
+│   ├── main.jsx         # Entry point for the application
+│   └── index.css        # Global CSS styles
+├── package.json
+├── vite.config.js
+└── README.md
+
+
+
+*/
